f69028ae11852e85a6cd8439d90494ddebdbe472
Tirth <tirthcoder@gmail.com>
2025-08-14 12:24:17 +0530

    Patterns added

---
+++ Foundation/Patterns/L6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class L6 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int st = n / 2 + 1, sp = n / 2;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= st; j++) {
+                System.out.print("* ");
+            }
+
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= st; j++) {
+                System.out.print("* ");
+            }
+            if (i <= (n / 2)) {
+                sp += 2;
+                st--;
+
+            } else {
+                sp -= 2;
+                st++;
+            }
+            System.out.println();
+        }
+    }
+}
